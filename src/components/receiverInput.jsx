04b0c2966e2ddba84b03d87f01a92a4bf68cea9c
arhamabeer <arhamabeerahmed@gmail.com>
2021-05-09 22:49:45 +0500

    fourth commit

--- src/components/receiverInput.jsx
+++ src/components/receiverInput.jsx
@@ -11,12 +11,17 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ReceiverInput() {
+
+export default function ReceiverInput({func}) {
   const classes = useStyles();
+
+  const handleChange = (e) => {
+    func(e)
+  }
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="standard-basic" label="Enter Receiver's Account Number" />
+      <TextField onChange={(e) => handleChange(e.target.value)} id="standard-basic" label="Enter Receiver's Account Number" />
     </form>
   );
 }
--- src/components/transferMoney.jsx
+++ src/components/transferMoney.jsx
@@ -1,58 +1,110 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReceiverInput from './receiverInput'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { connect } from 'react-redux'
+import { getFbUsers } from '../store/action'
 
 
-export default function TransferMoney() {
 
+
+function TransferMoney({ customers, getFbUsers }) {
+
+    useEffect(() => {
+        const getUsers = async () => {
+            await getFbUsers();
+        }
+        getUsers();
+    }, []);
+
+    var accounts = customers.map((v) => {
+        return v.account_no
+    })
+
+    const [acc, setAcc] = useState('')
+
+    const getInput = (e) => {
+        setAcc(e)
+    }
+    var checker = ''
+    const handleSearchClick = (e) => {
+        var searched = accounts.includes(acc)
+        
+        checker = searched
+        if (searched) {
+            var check = ''
+            customers.map((v) => {
+                return v.account_no === acc ? check = v : 'no'
+            })
+            console.log(check)
+        }
+        // console.log(acc, searched)
+    }
+
+    // console.log('running...', acc)
     return (
         <div className='transferMoneyCss'>
-            <ReceiverInput />
-            <Button variant='contained' color='primary' style={{ width: 200, height: 45, fontSize: 18 }}  >Search</Button>
-            <div className='transferInfo'>
-                <TextField
-                    style={{ width: '60ch', margin: 5 }}
-                    disabled
-                    id="filled-disabled"
-                    label="Account Number"
-                    defaultValue="090078601"
-                    variant="filled"
-                />
-                <TextField
-                    style={{ width: '60ch', margin: 5 }}
-                    disabled
-                    id="filled-disabled"
-                    label="Customer Name"
-                    defaultValue="AAA"
-                    variant="filled"
-                />
-                <TextField
-                    style={{ width: '60ch', margin: 5 }}
-                    disabled
-                    id="filled-disabled"
-                    label="email id"
-                    defaultValue="aaa@gmail.com"
-                    variant="filled"
-                />
-                <TextField
-                    style={{ width: '60ch', margin: 5 }}
-                    disabled
-                    id="filled-disabled"
-                    label="branch city"
-                    defaultValue="Dubai"
-                    variant="filled"
-                />
-                <TextField
-                    required
-                    style={{ width: '60ch', margin: 5 }}
-                    id="filled-required"
-                    label="Enter Money to Transfer"
-                    placeholder='0'
-                    variant="filled"
-                />
-                <Button variant='contained' color='secondary' style={{ width: 200, height: 45, fontSize: 18 }}  >Transfer</Button>
-            </div>
+            <ReceiverInput func={getInput} />
+            <Button onClick={() => handleSearchClick()} variant='contained' color='primary' style={{ width: 200, height: 45, fontSize: 18 }}  >Search</Button>
+            {
+                checker ?
+                    <div className='transferInfo'>
+                        <TextField
+                            style={{ width: '60ch', margin: 5 }}
+                            disabled
+                            id="filled-disabled"
+                            label="Account Number"
+                            defaultValue="090078601"
+                            variant="filled"
+                        />
+                        <TextField
+                            style={{ width: '60ch', margin: 5 }}
+                            disabled
+                            id="filled-disabled"
+                            label="Customer Name"
+                            defaultValue="AAA"
+                            variant="filled"
+                        />
+                        <TextField
+                            style={{ width: '60ch', margin: 5 }}
+                            disabled
+                            id="filled-disabled"
+                            label="email id"
+                            defaultValue="aaa@gmail.com"
+                            variant="filled"
+                        />
+                        <TextField
+                            style={{ width: '60ch', margin: 5 }}
+                            disabled
+                            id="filled-disabled"
+                            label="branch city"
+                            defaultValue="Dubai"
+                            variant="filled"
+                        />
+                        <TextField
+                            required
+                            style={{ width: '60ch', margin: 5 }}
+                            id="filled-required"
+                            label="Enter Money to Transfer"
+                            placeholder='0'
+                            variant="filled"
+                        />
+                        <Button variant='contained' color='secondary' style={{ width: 200, height: 45, fontSize: 18 }}  >Transfer</Button>
+                    </div>
+                    : <h1>No Matches Found</h1>
+            }
         </div>
     )
 }
+
+
+const mapStateToProps = (state) => ({
+    customers: state.customers
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    getFbUsers: () => dispatch(getFbUsers())
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TransferMoney)
